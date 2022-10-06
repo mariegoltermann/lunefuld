f04f3e64458231f2b700accdc8df70b28a51d973
mariegoltermann <mari09a7@stud.kea.dk>
2022-10-06 10:58:30 +0200

    billeder

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 import "./style.css";
+
+const acceptCookie = document.querySelector("#accepter");
+const denyCookie = document.querySelector("#afvis");
+const cookieboks = document.querySelector("#cookieboks");
+
+acceptCookie.addEventListener("click", fjernCookieBoks);
+denyCookie.addEventListener("click", fjernCookieBoks);
+
+function fjernCookieBoks() {
+    cookieboks.classList.add("hidden");
+    sessionStorage-SVGTextPositioningElement("fjerncookieboks", true);
+}
+
+if (sessionStorage.getItem("fjerncookieboks")){
+    cookieboks.classList.add("hidden");
+}
